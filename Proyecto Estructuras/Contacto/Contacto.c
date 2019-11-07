05384e9ae6a42093598bf0bafbae60f70c50bbd2
Alan-Franisco <alanfcruzch99@gmail.com>
2019-11-06 21:56:40 -0600

    Agrege la funcion crear, revisarla
    En los "..." no se que poner exactamente

--- Proyecto Estructuras/Contacto/Contacto.c
+++ Proyecto Estructuras/Contacto/Contacto.c
@@ -2,5 +2,20 @@
 #include <stdlib.h>
 #include "ErrorCodes.h"
 
+typedef struct contacto{
+	char *nombre;
+	char *telefono;
+}t_contacto, *Contacto;
 
+int crearContacto(Contacto *nuevoContacto){
 
+contacto_temp = NULL;
+
+contacto_temp = (Contacto*) malloc (sizeof (t_contacto));
+contacto_temp->nombre = (...) malloc (sizeof 10 * (char));
+contacto_temp->telefono = (...) malloc (sizeof 10 * (char));
+
+nuevoContacto = contacto_temp;
+
+return ....
+}
