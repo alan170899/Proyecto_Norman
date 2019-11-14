1e206aa46ed23042696bbab54c42bb4d8b105805
Alan-Franisco <alanfcruzch99@gmail.com>
2019-11-14 16:05:09 -0600

    FUNCIONES AGREGAR, MOSTRAR Y ELIMMINAR AGREGADAS
    FALTA COMO LIMPIAR EL BUFFER

--- Proyecto Estructuras/Menu.c
+++ Proyecto Estructuras/Menu.c
@@ -1,8 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct contacto{
+	char *Nombre;
+	char *Telefono;
+	struct contacto *sig;
+	struct contacto *ant;
+};
+
+void Agregar_Cont(struct contacto *Agenda, char *Nombre, char *Telefono){
+	struct contacto *NuevoContacto = (struct contacto*) malloc (sizeof (struct contacto));
+	NuevoContacto->Nombre = Nombre;
+	NuevoContacto->Telefono = Telefono;
+	NuevoContacto->sig = NULL;
+	NuevoContacto->ant = NULL;
+	struct contacto *AUX1 = Agenda;
+	struct contacto *AUX2 = NULL;
+
+	if (AUX1 == NULL){
+		Agenda = NuevoContacto;
+	}
+	else{
+		while (AUX1 != NULL){
+			AUX2 = AUX1;
+			AUX1 = AUX1->sig;
+		}
+		AUX2->sig = NuevoContacto;
+		NuevoContacto->ant = AUX2;
+	}
+}
+
+struct contacto* Mostar_Ascendente(struct contacto *Agenda){
+	struct contacto *Aux = Agenda;
+	
+	while (Aux != NULL){
+	       	printf ("%s \n", Aux->Nombre);
+		printf ("%s \n", Aux->Telefono);
+		Aux = Aux->sig;
+	}
+
+	return Aux;
+}
+
+struct contacto* Mostar_Descendente(struct contacto *Agenda){
+        struct contacto *Aux1 = Agenda;
+	struct contacto *Aux2 = NULL;
+
+        while (Aux1 != NULL){
+                 Aux2 = Aux1;
+                 Aux1 = Aux1->sig;
+	}
+
+	while (Aux2 != NULL){
+		printf ("%s \n", Aux2->Nombre);
+                printf ("%s \n", Aux2->Telefono);
+		Aux2 = Aux2->ant;
+	}
+
+        return Aux2;
+}
+
+void Eliminar_contacto(struct contacto *Agenda, char *NuevoNombre, char *NuevoTelefono){
+	if (Agenda != NULL){
+		struct contacto *Aux_borrar = Agenda;
+		struct contacto *Aux2 = NULL;
+		
+		while ((Aux_borrar != NULL) && (Aux_borrar->Nombre != NuevoNombre) && (Aux_borrar->Telefono != NuevoTelefono));
+			Aux2 = Aux_borrar;
+			Aux_borrar = Aux_borrar->sig;
+			if (Aux_borrar == NULL){
+				printf ("No existe el elemento a borrar \n");
+			}
+			else if (Aux2 == NULL){
+				Agenda = Agenda->sig;
+				free(Aux_borrar);
+				printf("Elemento borrado \n");
+			}
+			else {
+				Aux2->sig = Aux_borrar->sig;
+				free(Aux_borrar);
+				printf("Elemento borrado \n");
+			}
+	}
+	else {
+		printf ("La Agenda esta vacia \n");
+	}
+}		       
+
+
 int main(){
   int opc;
+  struct contacto *Agenda = NULL;
+  char Nombre[20];
+  char Telefono[20];
   do{
   printf("Menu de agenda\n");
   printf("Que desea hacer?\n");
@@ -15,12 +105,25 @@ int main(){
   switch(opc){
 	case 1:
 		printf("Se entro correctamente a la primera opcion\n");
+		printf(" Introdusca un nombre : \n");
+		gets(Nombre);
+		printf(" Introdusca un telefono : \n");
+                gets(Telefono);
+		Agregar_Cont(Agenda,Nombre,Telefono);
 	break;
 	case 2:
 		printf("Se entro correctamente a la segunda opcion\n");
+		Mostar_Ascendente(Agenda);
+		Mostar_Descendente(Agenda);
 	break;
 	case 3:
 		printf("Se entro correctamente a la tercera opcion\n");
+		printf("Se entro correctamente a la primera opcion\n");
+                printf(" Introdusca un nombre : \n");
+                gets(Nombre);
+                printf(" Introdusca un telefono : \n");
+                gets(Telefono);
+		Eliminar_contacto(Agenda,Nombre,Telefono);
 	break;
 	case 4:
 		printf("Se entro correctamente a la cuarta opcion\n");
@@ -33,4 +136,5 @@ int main(){
 	break;
   } 
  }while(opc != 0);
+return 0;
 }
